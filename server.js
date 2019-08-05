66ca29ff546bbb4cf3f90328bfc3c4e6b6c4e63b
haimap <43215375+haimap@users.noreply.github.com>
2019-08-05 11:38:38 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+// init project
+var path = require('path');
+const accepts = require('accepts');
+var express = require('express');
+const eua = require('express-useragent');
+var app = express();
+
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+app.use(eua.express());
+
+// routing
+app.get("/", function (request, response) {
+  response.redirect('/api/track');
+  // response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/track', (req, res) => {
+  let headers = accepts(req);
+  res.json({
+    "ipaddress": req.ip,
+    "language": headers.languages()[0],
+    "software": req.useragent && req.useragent.source ? req.useragent.source.match(/\(([^)]+)\)/)[1] : ""
+  }).end();
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
